81009ec89d6c295b4d1e723795946d74d9c10ea9
MujtabaHafizi <muj.bdn@gmail.com>
2021-01-28 10:10:40 -0500

    Solar System Deployed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args) {
 
 
+        SolarSystem FactorX = new SolarSystem();
+        FactorX.Sun=1;
+        FactorX.Moon=3;
+        FactorX.Planets=2;
         
     }
 }
